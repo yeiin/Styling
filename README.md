d07f9af65c44acd3348f4a2f1c8bac8523f275dd
yeiin <yeini0417@gmail.com>
2022-02-02 00:33:24 +0900

    adding new styled components with div

--- README.md
+++ README.md
@@ -5,3 +5,5 @@ Goal: Add dynamic styling differently than css
 
 - trim() : removes excess white space at the beginning or the end.
 - inline style : The css code which is short in HTML code.
+
+- 
--- src/components/CourseGoals/CourseInput/CourseInput.js
+++ src/components/CourseGoals/CourseInput/CourseInput.js
@@ -1,7 +1,42 @@
 import React, { useState } from "react";
-
+import styled from "styled-components";
 import Button from "../../UI/Button/Button";
 import "./CourseInput.css";
+
+const FromControl = styled.div`
+.form-control {
+  margin: 0.5rem 0;
+}
+
+& label {
+  font-weight: bold;
+  display: block;
+  margin-bottom: 0.5rem;
+  color: ${props => props.invalid ? 'red' : 'black'};
+}
+
+& input {
+  display: block;
+  width: 100%;
+  border: 1px solid ${props => props.invalid ? 'red' : '#ccc'};
+  background: ${props => props.invalid ? '#ffd7d7' : 'transparent'};
+  font: inherit;
+  line-height: 1.5rem;
+  padding: 0 0.25rem;
+}
+
+& input:focus {
+  outline: none;
+  background: #fad0ec;
+  border-color: #8b005d;
+}
+
+&.invalid input {
+  border-color: red;
+  background: #ffd7d7;
+}
+
+`;
 
 const CourseInput = (props) => {
   const [enteredValue, setEnteredValue] = useState("");
@@ -25,17 +60,14 @@ const CourseInput = (props) => {
 
   return (
     <form onSubmit={formSubmitHandler}>
-      <div className="form-control">
-        <label style={{ color: !isValid ? "red" : "black" }}>Course Goal</label>
+      <FromControl invalid={!isValid}>
+        <label >Course Goal</label>
         <input
-          style={{
-            bordercolor: !isValid ? "red" : "#ccc",
-            background: !isValid ? "salmon" : "transparent",
-          }}
+          
           type="text"
           onChange={goalInputChangeHandler}
         />
-      </div>
+      </FromControl>
       <Button type="submit">Add Goal</Button>
     </form>
   );
